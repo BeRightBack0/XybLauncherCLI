0981e5400b108a539900ceb6407f1a68d35e0ccc
BeRightBack <BeRightBack0@users.noreply.github.com>
2024-12-10 18:03:28 +0100

    update the not working coblat download ok ok

--- Assets/DllsHandler.cs
+++ Assets/DllsHandler.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace XybLauncher;
 
-internal class DllsHandler
+public class DllsHandler
 {
 
+    public static void DownloadFile(string Url, string Path) => new WebClient().DownloadFile(Url, Path);
+
+    public static void DownloadFiles()
+    {
 
 
+        string appfolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\XybLauncher";
 
+        if (!File.Exists(appfolder + "\\Cobalt.dll"))
+            DownloadFile("https://www.dropbox.com/scl/fi/m996mhjy77qn2t3bfxq6l/Cobalt.dll?rlkey=6araxm5ngyznp4fmvxqtgm9a7&st=1o2rxx0l&dl=1", appfolder + "\\Cobalt.dll");
+
+
+    }
 
 
 }
